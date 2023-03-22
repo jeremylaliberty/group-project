e6401a7e728563a221092683c84f5f8454e395ba
Jeremy Laliberty <jlaliberty@wisc.edu>
2023-03-21 20:36:14 -0500

    added code; having issue importing  module

--- geospatial/app.js
+++ geospatial/app.js
@@ -1,2 +1,11 @@
 // use this file to test geospatial features of the site
 
+var current_location = turf.point([-77, 44]);
+        var polygon = turf.polygon([[
+        [-81, 41],
+        [-81, 47],
+        [-72, 47],
+        [-72, 41],
+        [-81, 41]
+    ]]);
+        console.log(turf.booleanPointInPolygon(current_location, polygon));
