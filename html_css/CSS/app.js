c11708ddd59f1e23f198fae2435c8ab0b726b8ec
aseibulescu <aseibulescu@wisc.edu>
2023-05-03 22:59:47 -0500

    actually fixed

--- html_css/CSS/app.js
+++ html_css/CSS/app.js
@@ -51,7 +51,7 @@ function alumniPage(){
   profile.classList.add('is-hidden');
   attendance.classList.add('is-hidden');
   alumni_page.classList.remove('is-hidden');
-  alumni_network.add('is-hidden');
+  alumni_network.classList.add('is-hidden');
 
 }
 
